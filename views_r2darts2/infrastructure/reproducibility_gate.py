725495e93e8f024168510e22bcb95428ff021641
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-07-11 21:17:53 +0200

    huber?

--- views_r2darts2/infrastructure/reproducibility_gate.py
+++ views_r2darts2/infrastructure/reproducibility_gate.py
@@ -343,7 +343,7 @@ class ReproducibilityGate:
             "PrismLoss": ["non_zero_threshold", "delta"],
             "SpotlightLoss": ["non_zero_threshold"],
             "SpotlightLossLogcosh": ["non_zero_threshold"],
-            "SpotlightLossHuber": ["delta", "non_zero_threshold"],
+            "SpotlightLossHuber": ["non_zero_threshold"],
             "SpotlightLossPowerLaw": ["non_zero_threshold"],
             "SpotlightLossAsinh": ["non_zero_threshold"],
             "CharbonnierLoss": [],

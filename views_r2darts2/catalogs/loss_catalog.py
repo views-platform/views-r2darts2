fa287117ed78503bec0f49ac84af0c640402c355
Dylan <52908667+smellycloud@users.noreply.github.com>
2026-08-13 15:23:42 +0200

    try not in memory preds

--- views_r2darts2/catalogs/loss_catalog.py
+++ views_r2darts2/catalogs/loss_catalog.py
@@ -85,6 +85,8 @@ class LossCatalog:
             "HuberLoss": torch.nn.HuberLoss,
             "SmoothL1Loss": torch.nn.SmoothL1Loss,
             "PoissonNLLLoss": torch.nn.PoissonNLLLoss,
+            "LogCoshLoss": LogCoshLoss,
+            "HuberLoss": HuberLoss,
         }
 
         if self.loss_name not in loss_classes:
